0318c807d4c9ee4f3d71fc15ad86e9ac7a581393
Ryan Soderberg <ryanjsoderberg@gmail.com>
2025-05-04 13:38:23 -0400

    voting logic

--- src/utils/match.ts
+++ src/utils/match.ts
@@ -1,6 +1,10 @@
 import { createServerFn } from "@tanstack/react-start";
+import { eq } from "drizzle-orm";
 import { sql } from "drizzle-orm";
+import { z } from "zod";
 import { db } from "~/db";
+import { calculateElo } from "./elo";
+import { match, outfitRating } from "~/db/schema";
 
 export const getPair = createServerFn().handler(async () => {
   const pair = await db.query.outfit.findMany({
@@ -15,3 +19,79 @@ export const getPair = createServerFn().handler(async () => {
     throw new Error("No pair found");
   }
 });
+
+export const vote = createServerFn()
+  .validator(
+    z.object({
+      winnerOutfitId: z.number(),
+      loserOutfitId: z.number(),
+    })
+  )
+  .handler(async ({ data }) => {
+    const { winnerOutfitId, loserOutfitId } = data;
+
+    try {
+      const result = await db.transaction(async (tx) => {
+        const [winner, loser] = await Promise.all([
+          tx.query.outfitRating.findFirst({
+            where: (outfitRating, { eq }) =>
+              eq(outfitRating.outfitId, winnerOutfitId),
+          }),
+          tx.query.outfitRating.findFirst({
+            where: (outfitRating, { eq }) =>
+              eq(outfitRating.outfitId, loserOutfitId),
+          }),
+        ]);
+
+        if (!winner || !loser) {
+          throw new Error(
+            `Outfit not found: ${winnerOutfitId}, ${loserOutfitId}`
+          );
+        }
+
+        const { RaPrime, RbPrime } = calculateElo(winner.rating, loser.rating);
+
+        const [matchRecord] = await tx
+          .insert(match)
+          .values({
+            outfit1Id: winnerOutfitId,
+            outfit2Id: loserOutfitId,
+            winnerId: winnerOutfitId,
+            loserId: loserOutfitId,
+            rating1Before: winner.rating,
+            rating2Before: loser.rating,
+            rating1After: RaPrime,
+            rating2After: RbPrime,
+          })
+          .returning();
+
+        const [updatedWinner] = await tx
+          .update(outfitRating)
+          .set({
+            rating: RaPrime,
+          })
+          .where(eq(outfitRating.outfitId, winnerOutfitId))
+          .returning();
+
+        const [updatedLoser] = await tx
+          .update(outfitRating)
+          .set({
+            rating: RbPrime,
+          })
+          .where(eq(outfitRating.outfitId, loserOutfitId))
+          .returning();
+
+        return { matchRecord, updatedWinner, updatedLoser };
+      });
+
+      const { matchRecord, updatedWinner, updatedLoser } = result;
+
+      return {
+        matchRecord,
+        newRatings: { winner: updatedWinner, loser: updatedLoser },
+      };
+    } catch (error) {
+      console.error(error);
+      throw new Error("Failed to vote");
+    }
+  });
