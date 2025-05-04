601e708368d347b2f4e8a29714681061b879c295
Ryan Soderberg <ryanjsoderberg@gmail.com>
2025-05-04 15:01:28 -0400

    error handling

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,10 +5,14 @@ import ws from "ws";
 
 import * as schema from "./schema";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
 // const sql = neon(process.env.DATABASE_URL!);
 // export const db = drizzle({ client: sql, schema });
 export const db = drizzle({
-  connection: process.env.DATABASE_URL!,
+  connection: process.env.DATABASE_URL,
   schema,
   ws,
 });
